8873df91c92f5256687d47f2d767c98392e157d5
Justin Pinkard <justin.d.pinkard@gmail.com>
2023-11-04 02:40:42 -0700

    Story 11

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+
+  return (
+    <>
+        <footer>
+            <p>Made by Justin Pinkard <span>
+                <a href="https://www.linkedin.com/in/jdpinkard" target="_blank" rel="noopener noreferrer">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/480px-LinkedIn_logo_initials.png" width="25px" alt="LinkedIn" />
+                </a></span>
+                <span> <a href="https://github.com/jdpinkard" target="_blank" rel="noopener noreferrer">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="25px" alt="GitHub" />
+                </a></span>
+            </p>
+        </footer>
+
+    </>
+  )
+}
+export default Footer
